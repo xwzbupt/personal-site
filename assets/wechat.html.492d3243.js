7a22585022e27dee9f3cfceb2cdf5840f2db7f5f
xwzbupt <xwze@bupt.cn>
2022-09-13 03:56:25 +0000

    Deploying to gh-pages from @ xwzbupt/personal-site@8bb688d517b4290acaeffbc5481f41ffdf6a394c 🚀

--- assets/wechat.html.492d3243.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as t,c}from"./app.5c0a6e97.js";const o={},s={src:"https://personal-site-pictures.oss-cn-beijing.aliyuncs.com/img/wechat.jpg",alt:"wechat",style:{zoom:"25%"}};function r(a,n){return t(),c("img",s)}var m=e(o,[["render",r],["__file","wechat.html.vue"]]);export{m as default};
