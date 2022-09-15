72f8d4bdc0d925af52d6b1fcb6681496adbf310d
xwzbupt <xwze@bupt.cn>
2022-09-16 02:41:13 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,5 @@
 import { defineUserConfig } from "vuepress";
-
+import { mdEnhancePlugin } from "vuepress-plugin-md-enhance";
 import theme from "./theme";
 
 export default defineUserConfig({
@@ -22,6 +22,47 @@ export default defineUserConfig({
   theme,
 
   plugins: [
-    
+    mdEnhancePlugin({
+      // 启用自定义容器
+      align: true,
+      attrs: true,
+      chart: true,
+      codetabs: true,
+      container: true,
+      demo: true,
+      echarts: true,
+      flowchart: true,
+      gfm: true,
+      imageSize: true,
+      include: true,
+      lazyLoad: true,
+      mark: true,
+      mermaid: true,
+      playground: {
+        presets: ["ts", "vue"],
+      },
+      presentation: {
+        plugins: ["highlight", "math", "search", "notes", "zoom"],
+      },
+      stylize: [
+        {
+          matcher: "Recommanded",
+          replacer: ({ tag }) => {
+            if (tag === "em")
+              return {
+                tag: "Badge",
+                attrs: { type: "tip" },
+                content: "Recommanded",
+              };
+          },
+        },
+      ],
+      sub: true,
+      sup: true,
+      tabs: true,
+      tex: true,
+      vpre: true,
+      vuePlayground: true,
+    }),
   ],
 });
