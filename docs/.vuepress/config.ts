53bc8e47f79df284371e0a2af9a56e94d0b7c76b
weiser <1134685563@qq.com>
2025-06-22 00:17:27 +0800

    trigger deploy

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,7 +8,9 @@ export default defineUserConfig({
 
   //如果是使用gh-pages布署，base应该用仓库名
   //base: "/personal-site/",
-  base: "/personal-site/",
+  //base: "/personal-site/",
+  // 如果你使用了自定义域名，必须是 /
+  base: "/",
   head: [
     [
       "link",
